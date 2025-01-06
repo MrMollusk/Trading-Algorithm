740b26fc718747318fbb68f7db8090ef008f42b9
MrMollusk <lccoleman@protonmail.com>
2025-01-06 20:54:00 +0000

    Delete API.h

--- API.h
+++
@@ -1,9 +0,0 @@
-#ifndef HEADER_H
-#define HEADER_H
-#include <string>
-#include <vector>
-
-std::string fetchApiData(const std::string& url);
-std::vector <float> jsonExtraction(const std::string& response, const std::string extractor);
-
-#endif
